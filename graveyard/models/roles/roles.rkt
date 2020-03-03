c2cb8fffa1d8c92a672e50ce16dced558d7e8857
Thea Leake <thea.leake@gmail.com>
2020-03-02 23:28:20 -0800

    Consolidated role model into single module again.

--- graveyard/models/roles/roles.rkt
+++ graveyard/models/roles/roles.rkt
@@ -35,5 +35,116 @@
          flip
          (struct-out cell))
 
-(require "private/definitions.rkt"
-         "private/memoized.rkt")
+
+(require tmemoize)
+(require/typed
+    racket/list
+  [flatten (-> (Listof(Listof String)) (Listof String))]
+  [make-list (-> Integer String (Listof Role))]
+  [index-of (-> (Listof Role) Role Integer)])
+
+
+(define-type None False)
+
+(define none : None #f)
+
+(define-type Player (U String None))
+
+(define-type Role String)
+
+
+(struct cell
+  ([player : Player]
+   [revealed? : Boolean]
+   [role : Role]
+   [empty? : Boolean])
+  #:transparent)
+
+
+;; roles
+(define leader : Role "Lich")
+(define advisor : Role "Vampire")
+(define elephant : Role "Zombie")
+(define chariot : Role "Ghoul")
+(define horse : Role "Skeleton")
+(define cannon : Role "Wraith")
+(define pawn : Role "Poltergeist")
+(define empty : Role "Empty")
+
+
+(: players (Pair Player Player))
+(define players
+  (cons "Orange" "Purple"))
+
+
+(define none-role
+  (cell none       ;; player
+        #t         ;; revealed
+        empty      ;; role
+        #t))       ;; empty
+
+
+;; it is worth noting that this is not referenced when the cannon is capturing
+;; cannons can capture any unit, and any unit except soldier can capture the cannon
+(: role-hierarchy (Listof Role))
+(define role-hierarchy
+  (list leader advisor elephant chariot horse cannon pawn empty))
+
+
+;; number of pieces that can be involve in a cannon move
+;; cannon, piece cannon jumps over, piece cannon takes
+(define cannon-max-pieces : Integer 3)
+
+
+
+(: player-start-roles (Listof String))
+(define player-start-roles
+  (flatten
+   (list (make-list 1 leader)
+         (make-list 2 advisor)
+         (make-list 2 elephant)
+         (make-list 2 chariot)
+         (make-list 2 horse)
+         (make-list 5 pawn)
+         (make-list 2 cannon))))
+
+(memoized
+ (: hierarchy-value (-> Role Integer))
+ (define (hierarchy-value role)
+   (index-of role-hierarchy role)))
+
+
+;; the memoized lambda lets us memoize the the pieces of the same data
+;; so they share the same obj id - which lets us memoize more effectively
+;; further down the line w/ only obj id checks
+;; using a memoized lambda instead of define/memo to allow these object to be cleaned up by GC when no longer used.
+(: piece-maker (-> (-> Player Role cell)))
+(define (piece-maker)
+  (memoize (lambda ([player : Player] [role : Role])
+             (cell player     ;; player
+                   #f         ;; revealed
+                   role       ;;
+                   #f))))      ;; empty
+
+
+(: player-roles (-> Player (Listof cell)))
+(define (player-roles team)
+  (let ([mkpiece : (-> Player Role cell) (piece-maker)])
+    (map (lambda ([role : Role]) (mkpiece team role))
+         player-start-roles)))
+
+
+(memoized
+ (: toggle-player (-> Player Player))
+ (define (toggle-player player)
+   (if (eq? player (car players))
+       (cdr players)
+       (car players))))
+
+
+(memoized
+ (: flip (-> cell cell))
+ (define(flip piece)
+   (struct-copy cell piece
+                [revealed? #t])))
+
